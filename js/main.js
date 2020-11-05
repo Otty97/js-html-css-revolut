fa832ee18351d1b58ab8824af845d87deb52bb72
Othman Taik <taikothman1997@gmail.com>
2020-11-05 11:11:28 +0100

    Js Fixed

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 $(document).ready(
   function() {
+      //Definisco la funzione che si attivera con hover
     $(".parent-dropdown").hover(function() {
       $(this).find('ul').slideToggle();
     });
+
     $(".parent-dropdown").hover(
+
       function() {
         $(this).find("i").removeClass("fa-chevron-down");
         $(this).find("i").addClass("fa-chevron-up");
-
       },
+      
       function() {
         $(this).find("i").removeClass("fa-chevron-down");
         $(this).find("i").addClass("fa-chevron-up");
